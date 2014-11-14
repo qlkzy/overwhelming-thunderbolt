6be6c79c7e2e71fd95c0d77bd738d2f0f362f690
David Morris <davidrowlandmorris@gmail.com>
2014-11-14 23:55:38 +0000

    Spawn enemies

--- main.lua
+++ main.lua
@@ -8,6 +8,7 @@ local player
 local bullets = {}
 local enemies = {}
 local barriers = {}
+local max_enemies = 1
 
 function love.load()
     love.window.setTitle("Overwhelming Thunderbolt")
@@ -16,14 +17,12 @@ function love.load()
     love.graphics.setBackgroundColor(100, 100, 100)
     cursor = love.mouse.newCursor("crosshair.png", 10, 10)
     love.mouse.setCursor(cursor)
-    table.insert(enemies, Enemy.new(10, 10))
     table.insert(barriers, Barrier.new(100, 10, 105, 100))
 end
 
 function love.update(dt)
-    -- simulate the passing of time---<dt> is the
-    -- time (fraction of a second) that has passed
-    -- since the last call to love.update
+    local enemy_count = 0
+
     player:update(dt)
     local pbb = player:boundingBox()
     for _, bullet in pairs(bullets) do
@@ -53,6 +52,9 @@ function love.update(dt)
         end
         if enemy:isDead() then
             enemies[ke] = nil
+            max_enemies = max_enemies + 1
+        else
+            enemy_count = enemy_count + 1
         end
     end
 
@@ -68,6 +70,25 @@ function love.update(dt)
             player.x = player.x + vec.dx
             player.y = player.y + vec.dy
         end
+    end
+
+    if enemy_count < max_enemies then
+        local edge = math.random(4)
+        local max_x = love.graphics.getWidth()
+        local max_y = love.graphics.getHeight()
+        local ran_x = math.random(max_x)
+        local ran_y = math.random(max_y)
+        local enemy
+        if edge == 1 then
+            enemy = Enemy.new(0, ran_y)
+        elseif edge == 2 then
+            enemy = Enemy.new(ran_x, 0)
+        elseif edge == 3 then
+            enemy = Enemy.new(max_x, ran_y)
+        else
+            enemy = Enemy.new(ran_x, max_y)
+        end
+        table.insert(enemies, enemy)
     end
 end
 
