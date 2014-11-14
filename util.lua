6cfca4b4b55cb208adce8440a7c79ae4adec6c06
David Morris <davidrowlandmorris@gmail.com>
2014-11-14 23:20:41 +0000

    Add 'unOverlap' function

--- util.lua
+++ util.lua
@@ -7,11 +7,30 @@ function util.within(point, box)
         point.y <= box.y1
 end
 
-function util.overlap(box0, box1)
-    return util.witin({x = box0.x0, y = box0.y0}, box1) or
-        util.within({x = box0.x0, y = box0.y1}, box1) or 
-        util.within({x = box0.x1, y = box0.y0}, box1) or
-        util.within({x = box0.x1, y = box0.y1}, box1)
+function util.overlap(boxA, boxB)
+    return util.within({x = boxA.x0, y = boxA.y0}, boxB) or
+        util.within({x = boxA.x0, y = boxA.y1}, boxB) or 
+        util.within({x = boxA.x1, y = boxA.y0}, boxB) or
+        util.within({x = boxA.x1, y = boxA.y1}, boxB)
+end
+
+-- vector to move boxA by to avoid overlapping boxB
+function util.unOverlap(boxA, boxB)
+    if util.within({x = boxA.x0, y = boxA.y0}, boxB) then
+        return {dx = boxB.x1 - boxA.x0,
+                dy = boxB.y1 - boxA.y0}
+    elseif util.within({x = boxA.x0, y = boxA.y1}, boxB) then
+        return {dx = boxB.x1 - boxA.x0,
+                dy = boxB.y0 - boxA.y1}
+    elseif util.within({x = boxA.x1, y = boxA.y0}, boxB) then
+        return {dx = boxB.x0 - boxA.x1,
+                dy = boxB.y1 - boxA.y0}
+    elseif util.within({x = boxA.x1, y = boxA.y1}, boxB) then
+        return {dx = boxB.x0 - boxA.x1,
+                dy = boxB.y0 - boxA.y1}
+    else
+        return {dx = 0, dy = 0}
+    end
 end
 
 return util
